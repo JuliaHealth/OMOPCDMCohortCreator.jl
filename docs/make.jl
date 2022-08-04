5bcfc02aed4caa6e717cb73b66c024c1f77532d6
TheCedarPrince <jacobszelko@gmail.com>
2022-08-04 12:41:38 -0400

    First pass at setting up Documenter for package

---
+++ docs/make.jl
@@ -0,0 +1,25 @@
+using OMOPCDMCohortCreator
+using Documenter
+
+makedocs(;
+    modules = [OMOPCDMCohortCreator],
+    authors = "Jacob Zelko (aka TheCedarPrince) <jacobszelko@gmail.com> and contributors",
+    repo = "https://github.com/JuliaHealth/OMOPCDMCohortCreator.jl/blob/{commit}{path}#L{line}",
+    sitename = "OMOPCDMCohortCreator.jl",
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://JuliaHealth.github.io/OMOPCDMCohortCreator.jl",
+        assets = String[],
+        edit_branch = "main",
+        footer = "Created by [Jacob Zelko](https://jacobzelko.com). Funded through [Georgia Tech Research Institute](https://www.gtri.gatech.edu). [License](https://github.com/JuliaHealth/OMOPCDMCohortCreator.jl/blob/main/LICENSE)"
+    ),
+    pages = [
+        "Home" => "index.md"
+    ],
+)
+
+deploydocs(;
+    repo = "github.com/JuliaAnimators/Javis.jl",
+    push_preview = true,
+    devbranch = "main",
+)
---
+++ docs/src/index.md
@@ -0,0 +1,16 @@
+# Welcome to the `OMOPCDMCohortCreator.jl` Docs!
+
+Test page - here is a :smiley: 
+
+Here is some code:
+
+```
+function hello()
+	println("world")
+end
+```
+
+Here is some math:
+
+$$2\pi$$
+
