ac7862eb51476d720e63215004bba8c06304edfc
TheCedarPrince <jacobszelko@gmail.com>
2022-08-05 16:11:48 -0400

    Added new deps Dates and TimeZones for time management

--- src/OMOPCDMCohortCreator.jl
+++ src/OMOPCDMCohortCreator.jl
@@ -1,6 +1,9 @@
 module OMOPCDMCohortCreator
 
+#TODO: Bump minor version due to new dependencies added
+
 using DataFrames
+using Dates
 using DBInterface
 using FunSQL:
     SQLTable,
@@ -22,6 +25,7 @@ using FunSQL:
     LeftJoin,
     reflect
 using Memoization
+using TimeZones
 
 include("caching_functions/getters.jl")
 include("caching_functions/filters.jl")
