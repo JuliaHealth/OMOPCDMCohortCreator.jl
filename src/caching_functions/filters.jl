79441f36e3c4e42517c9a25060b338c128d850d9
TheCedarPrince <jacobszelko@gmail.com>
2022-08-05 17:50:59 -0400

    Removed AgeGroupFilter in favor of supporting GetPatientAgeGroup and fixed docstrings

--- src/caching_functions/filters.jl
+++ src/caching_functions/filters.jl
@@ -1,11 +1,13 @@
 """
-VisitFilterPersonIDs(visit_codes; tab::SQLTable = visit_occurrence)
+VisitFilterPersonIDs(visit_codes, conn; tab::SQLTable = visit_occurrence)
 
-Given a list of visit concept IDs, `visit_codes`  return from the database individuals having at least one entry in the Visit Occurrence table matching at least one of the provided visit types.
+Given a list of visit concept IDs, `visit_codes` return from the database patients matching at least one of the provided visit codes from the Visit Occurrence table.
 
 # Arguments:
 
 - `visit_codes` - a vector of `visit_concept_id`'s; must be a subtype of `Integer`
+
+`conn` - database connection using DBInterface
 
 # Keyword Arguments:
 
@@ -28,13 +30,15 @@ Given a list of visit concept IDs, `visit_codes`  return from the database indiv
 end
 
 """
-ConditionFilterPersonIDs(condition_codes; tab::SQLTable = condition_occurrence)
+ConditionFilterPersonIDs(condition_codes, conn; tab::SQLTable = condition_occurrence)
 
 Given a list of condition concept IDs, `condition_codes`, return from the database individuals having at least one entry in the Condition Occurrence table matching at least one of the provided condition types.
 
 # Arguments:
 
 - `condition_codes` - a vector of `condition_concept_id`'s; must be a subtype of `Integer`
+
+`conn` - database connection using DBInterface
 
 # Keyword Arguments:
 
@@ -60,13 +64,15 @@ Given a list of condition concept IDs, `condition_codes`, return from the databa
 end
 
 """
-RaceFilterPersonIDs(race_codes; tab::SQLTable = person)
+RaceFilterPersonIDs(race_codes, conn; tab::SQLTable = person)
 
 Given a list of condition concept IDs, `race_codes`, return from the database individuals having at least one entry in the Person table matching at least one of the provided race types.
 
 # Arguments:
 
 - `race_codes` - a vector of `race_concept_id`'s; must be a subtype of `Integer`
+
+`conn` - database connection using DBInterface
 
 # Keyword Arguments:
 
@@ -89,13 +95,15 @@ Given a list of condition concept IDs, `race_codes`, return from the database in
 end
 
 """
-GenderFilterPersonIDs(gender_codes; tab::SQLTable = visit_occurrence)
+GenderFilterPersonIDs(gender_codes, conn; tab::SQLTable = visit_occurrence)
 
 Given a list of visit concept IDs, `gender_codes` return from the database individuals having at least one entry in the Person table matching at least one of the provided gender types.
 
 # Arguments:
 
 - `visit_codes` - a vector of `gender_concept_id`'s; must be a subtype of `Integer`
+
+`conn` - database connection using DBInterface
 
 # Keyword Arguments:
 
@@ -118,13 +126,15 @@ Given a list of visit concept IDs, `gender_codes` return from the database indiv
 end
 
 """
-StateFilterPersonIDs(states; tab::SQLTable = location, join_tab::SQLTable = person)
+StateFilterPersonIDs(states, conn; tab::SQLTable = location, join_tab::SQLTable = person)
 
 Given a list of states, `states`, return from the database individuals found in the provided state list.
 
 # Arguments:
 
 - `states` - a vector of state abbreviations; must be a subtype of `AbstractString`
+
+`conn` - database connection using DBInterface
 
 # Keyword Arguments:
 
@@ -149,61 +159,4 @@ Given a list of states, `states`, return from the database individuals found in 
 
 end
 
-"""
-AgeGroupFilterPersonIDs(age_groupings; tab::SQLTable = location, join_tab::SQLTable = observation_period)
-
-Finds all individuals in age groups as specified by `age_groupings`.
-
-# Arguments:
-
-- `age_groupings` - a vector of age groups of the form `[[10, 19], [20, 29],]` denoting an age group of 10 - 19 and 20 - 29 respectively; age values must subtype of `Integer`
-
-# Keyword Arguments:
-
-- `tab::SQLTable` - the `SQLTable` representing the Person table; default `person`
-- `join_tab::SQLTable` - the `SQLTable` representing the Observation Period table; default `observation_period`
-
-# Returns
-
-- `ids::Vector{Int64}` - the list of persons resulting from the filter
-"""
-@memoize Dict function AgeGroupFilterPersonIDs(age_groupings, conn; tab = person, join_tab = observation_period)
-
-    age_arr = []
-    age_ranges = []
-    for grp in age_groupings
-        push!(age_arr, Get.age .< grp[2] + 1)
-        push!(age_arr, "$(grp[1]) - $(grp[2])")
-        push!(age_ranges, "$(grp[1]) - $(grp[2])")
-    end
-
-    ids =
-        From(person) |>
-        LeftJoin(
-            :observation_group => From(join_tab) |> Group(Get.person_id),
-            on = Get.person_id .== Get.observation_group.person_id,
-        ) |>
-        Select(
-            Get.person_id,
-            Fun.make_date(Get.year_of_birth, Get.month_of_birth, Get.day_of_birth) |>
-            As(:dob), #BUG: This only works for PostgreSQL, MySQL right now; breaks on SQLite.
-            #TODO: Refactor how to get the date agnostic from implementation: https://www.sqltutorial.org/sql-date-functions/sql-convert-string-to-date-functions/
-            Get.observation_group |>
-            Agg.max(Get.observation_period_end_date) |>
-            As(:record),
-        ) |>
-        Select(
-            Get.person_id,
-            :age => Fun.date_part("year", Fun.age(Get.record, Get.dob)),
-        ) |>
-        Define(:age_group => Fun.case(age_arr...)) |>
-        Where(Fun.in(Get.age_group, age_ranges...)) |>
-        Select(Get.person_id) |>
-        q -> render(q, dialect = dialect) |>
-        x -> DBInterface.execute(conn, String(x)) |> DataFrame
-
-    return convert(Vector{Int}, ids.person_id)
-
-end
-
-export VisitFilterPersonIDs, ConditionFilterPersonIDs, RaceFilterPersonIDs, GenderFilterPersonIDs, StateFilterPersonIDs, AgeGroupFilterPersonIDs
+export VisitFilterPersonIDs, ConditionFilterPersonIDs, RaceFilterPersonIDs, GenderFilterPersonIDs, StateFilterPersonIDs
