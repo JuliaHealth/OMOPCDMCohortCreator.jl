938427b463cdb99e2bdcc11175ff183ea6d46627
Chinta <vchinta6@icl.gtri.org>
2022-10-06 15:12:39 -0400

    Added test for filters.jl

--- test/sqlite/filters.jl
+++ test/sqlite/filters.jl
@@ -3,7 +3,7 @@
 labels: tests, good first issue
 =#
 @testset "VisitFilterPersonIDs Tests" begin
-    #9201 was the only visit_concept)id?
+    #9201 was the only visit_concept id?
     VisitFilterPersonIDs([9201.0], sqlite_conn)
     test_person_ids = [1, 2, 3, 5, 32, 35, 36, 42, 61, 80]
 
@@ -16,6 +16,15 @@ end
 labels: tests, good first issue
 =#
 @testset "ConditionFilterPersonIDs Tests" begin
+    #test for 192671.0 - singe condition code
+    test_person_ids_single = [3, 32, 35, 61, 80, 99, 115, 116, 133, 135]
+    res =  sort(ConditionFilterPersonIDs([192671.0], sqlite_conn))
+    @test test_person_ids_single == res[1:10]
+
+    #test for multiple condition codes - 28060 and 192671
+    test_person_ids_multiple = [1, 3, 5, 9, 11, 12, 17, 18, 19, 32]
+    res_multiple = sort(ConditionFilterPersonIDs([192671.0, 28060.], sqlite_conn))[1:10]
+    @test test_person_ids_multiple == res_multiple[1:10]
 	
 end
 
