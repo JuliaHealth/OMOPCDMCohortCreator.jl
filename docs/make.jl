5c94a73de0b8eb3a9ca422af1f35299f79789c28
TheCedarPrince <jacobszelko@gmail.com>
2022-10-23 16:27:59 -0400

    Clarified links for tutorials

--- docs/make.jl
+++ docs/make.jl
@@ -17,7 +17,9 @@ makedocs(;
         "Home" => "index.md",
         "Tutorials" => [
                         "tutorials.md",
-                        "beginner_tutorial.md"],
+                        "tutorials/beginner_tutorial.md",
+                        "tutorials/j_tutorial.md"
+                       ],
         "API" => "api.md"
     ],
 )
