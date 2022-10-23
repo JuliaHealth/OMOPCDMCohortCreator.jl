37c5a979f38b4d8b66a95c71aea60727be3d202d
TheCedarPrince <jacobszelko@gmail.com>
2022-10-23 03:39:52 -0400

    Fixed deprecation warning

--- docs/make.jl
+++ docs/make.jl
@@ -10,7 +10,6 @@ makedocs(;
         prettyurls = get(ENV, "CI", "false") == "true",
         canonical = "https://JuliaHealth.github.io/OMOPCDMCohortCreator.jl",
         assets = String[],
-        edit_branch = "main",
         edit_link = "dev",
 	footer = "Created by [Jacob Zelko](https://jacobzelko.com) & [Georgia Tech Research Institute](https://www.gtri.gatech.edu). [License](https://github.com/JuliaHealth/OMOPCDMCohortCreator.jl/blob/main/LICENSE)"
     ),
