e0341a4eff982de36f9af4439e0a7ebafbccd257
VarshC <49307186+VarshC@users.noreply.github.com>
2023-02-22 10:32:17 -0500

    issue #44 add GetVisitDate (#45)
    * added a function GetVisitDate and its tests. Also fixed test for GetVisitConcept
    
    * Removed SQLite dep
    
    * Added and updated docstrings
    
    ---------
    
    Co-authored-by: Varshini Chinta <vchinta6@icl.gtri.org>
    Co-authored-by: TheCedarPrince <jacobszelko@gmail.com>

--- src/getters.jl
+++ src/getters.jl
@@ -919,4 +919,74 @@ function GetVisitConcept(
 
 end
 
-export GetDatabasePersonIDs, GetPatientState, GetPatientGender, GetPatientRace, GetPatientAgeGroup, GetPatientVisits, GetMostRecentConditions, GetMostRecentVisit, GetVisitCondition, GetPatientEthnicity, GetDatabaseYearRange, GetVisitPlaceOfService, GetVisitConcept
+"""
+GetVisitDate(visit_occurrence_id; interval::Symbol = :start, tab = visit_occurrence)
+
+This function queries a database for the start or end date of the visit occurrence associated with the given `visit_occurrence_id` or list of `visit_occurrence_id`'s.
+
+# Arguments:
+- `visit_occurrence_id`: A single `visit_occurrence_id` or a vector of `visit_occurrence_id`'s to query for.
+
+- `conn` - database connection using DBInterface
+
+# Keyword Arguments
+
+- `interval`: A keyword argument that determines whether to query for the visit start date (`interval=:start`) or the visit end date (`interval=:end`). Default value is `interval=:start`.
+
+# Returns:
+A dataframe with two columns: `visit_occurrence_id` and either `visit_start_date` or `visit_end_date`, depending on the value of the `interval` argument.
+"""
+function GetVisitDate(
+    visit_occurrence_ids,
+    conn;
+    interval::Symbol = :start,
+    tab=visit_occurrence
+)
+    df = DBInterface.execute(conn, GetVisitDate(visit_occurrence_ids; interval, tab=tab)) |> DataFrame
+    return df
+end
+
+"""
+GetVisitDate(visit_occurrence_id; interval::Symbol = :start, tab = visit_occurrence)
+
+Produces SQL statement that, given a list of visit IDs, finds the visit start or end date.
+
+# Arguments:
+
+- `visit_occurrence_id`: A single `visit_occurrence_id` or a vector of `visit_occurrence_id`'s to query for.
+
+# Keyword Arguments
+
+- `interval`: A keyword argument that determines whether to query for the visit start date (`interval=:start`) or the visit end date (`interval=:end`). Default value is `interval=:start`.
+
+# Returns:
+
+A dataframe with two columns: `visit_occurrence_id` and either `visit_start_date` or `visit_end_date`, depending on the value of the `interval` argument.
+"""
+function GetVisitDate(
+    visit_occurrence_id;
+    interval::Symbol = :start,
+    tab=visit_occurrence
+)
+    if (interval == :start)
+        sql =
+            From(tab) |>
+            Where(Fun.in(Get.visit_occurrence_id, visit_occurrence_id...)) |>
+            Select(Get.visit_occurrence_id, Get.visit_start_date) |>
+            q -> render(q, dialect=dialect)
+        return String(sql)
+    elseif (interval == :end)
+        sql =
+        From(tab) |>
+        Where(Fun.in(Get.visit_occurrence_id, visit_occurrence_id...)) |>
+        Select(Get.visit_occurrence_id, Get.visit_end_date) |>
+        q -> render(q, dialect=dialect)
+        return String(sql)
+    else
+        return "NA"
+    end
+
+end
+
+
+export GetDatabasePersonIDs, GetPatientState, GetPatientGender, GetPatientRace, GetPatientAgeGroup, GetPatientVisits, GetMostRecentConditions, GetMostRecentVisit, GetVisitCondition, GetPatientEthnicity, GetDatabaseYearRange, GetVisitPlaceOfService, GetVisitConcept, GetVisitDate
