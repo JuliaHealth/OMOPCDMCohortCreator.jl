779d21fa819af4dac59fd9fffda83eea768604cd
TheCedarPrince <jacobszelko@gmail.com>
2023-02-22 10:14:52 -0500

    Added and updated docstrings

--- src/getters.jl
+++ src/getters.jl
@@ -743,16 +743,21 @@ function GetVisitCondition(
 end
 
 """
-GetVisitDate(visit_occurrence_id; interval::Symbol = :start, tab = visit_occurrence)
+GetVisitCondition(visit_ids; tab = visit_occurrence)
 
-This function queries a database for the start or end date of the visit occurrence associated with the given `visit_occurrence_id` or list of `visit_occurrence_id`'s.
+Produces SQL statement that, given a list of `visit_id`'s, finds the conditions diagnosed associated with that visit.
 
 # Arguments:
-- `visit_occurrence_id`: A single `visit_occurrence_id` or a vector of `visit_occurrence_id`'s to query for.
-- `interval`: A keyword argument that determines whether to query for the visit start date (`interval=:start`) or the visit end date (`interval=:end`). Default value is `interval=:start`.
 
-# Returns:
-A dataframe with two columns: `visit_occurrence_id` and either `visit_start_date` or `visit_end_date`, depending on the value of the `interval` argument.
+- `visit_ids` - list of `visit_id`'s; each ID must be of subtype `Integer`
+
+# Keyword Arguments:
+
+- `tab` - the `SQLTable` representing the Condition Occurrence table; default `condition_occurrence`
+
+# Returns
+
+- `df::DataFrame` - a two column `DataFrame` comprised of columns: `:visit_occurrence_id` and `:condition_concept_id`
 """
 function GetVisitCondition(
     visit_ids;
@@ -921,12 +926,16 @@ This function queries a database for the start or end date of the visit occurren
 
 # Arguments:
 - `visit_occurrence_id`: A single `visit_occurrence_id` or a vector of `visit_occurrence_id`'s to query for.
+
+- `conn` - database connection using DBInterface
+
+# Keyword Arguments
+
 - `interval`: A keyword argument that determines whether to query for the visit start date (`interval=:start`) or the visit end date (`interval=:end`). Default value is `interval=:start`.
 
 # Returns:
 A dataframe with two columns: `visit_occurrence_id` and either `visit_start_date` or `visit_end_date`, depending on the value of the `interval` argument.
 """
-
 function GetVisitDate(
     visit_occurrence_ids,
     conn;
@@ -937,8 +946,23 @@ function GetVisitDate(
     return df
 end
 
+"""
+GetVisitDate(visit_occurrence_id; interval::Symbol = :start, tab = visit_occurrence)
 
+Produces SQL statement that, given a list of visit IDs, finds the visit start or end date.
 
+# Arguments:
+
+- `visit_occurrence_id`: A single `visit_occurrence_id` or a vector of `visit_occurrence_id`'s to query for.
+
+# Keyword Arguments
+
+- `interval`: A keyword argument that determines whether to query for the visit start date (`interval=:start`) or the visit end date (`interval=:end`). Default value is `interval=:start`.
+
+# Returns:
+
+A dataframe with two columns: `visit_occurrence_id` and either `visit_start_date` or `visit_end_date`, depending on the value of the `interval` argument.
+"""
 function GetVisitDate(
     visit_occurrence_id;
     interval::Symbol = :start,
